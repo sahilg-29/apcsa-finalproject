6eb3136ec0f7226c45de32eb735da217e493db8e
sahilg-29 <sahilgwalani2009@gmail.com>
2026-04-23 09:45:10 -0700

    Removed override
    removed override

--- src/Habit.java
+++ src/Habit.java
@@ -43,7 +43,6 @@ public abstract class Habit {
 
     public abstract String getCategoryLabel();
 
-    @Override
     public String toString() {
         return habitTitle + " (created " + creationDate + ", goal " + goalDayCount + " days)";
     }
